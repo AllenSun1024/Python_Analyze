a27f90a904186035e16bff18044f4946f6f17f41
QingYiSun <nju15sc@gmail.com>
2022-03-14 14:35:38 +0800

    Case 27 done.

---
+++ Intra_Method_Analysis/Code/evaluate/TestCaseEvaluation.py
@@ -0,0 +1,12 @@
+def scan_two_sets(source, target):
+    pass
+
+
+def main():
+    source = "/home/allen/DL_API/Static_Analysis/Python_Analyze/Intra_Method_Analysis/TestCaseSet"
+    target = "/home/allen/DL_API/Static_Analysis/Python_Analyze/Intra_Method_Analysis/TestResult"
+    scan_two_sets(source=source, target=target)
+
+
+if __name__ == "__main__":
+    main()
